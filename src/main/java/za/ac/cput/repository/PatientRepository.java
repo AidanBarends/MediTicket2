0ed47ab0e6b57df5c4c958a41ef0289076f121fe
aidan-g-barends <aidanbarends95@gmail.com>
2026-06-29 21:05:42 +0200

    Implemented PatientRepository

--- src/main/java/za/ac/cput/repository/PatientRepository.java
+++ src/main/java/za/ac/cput/repository/PatientRepository.java
@@ -1,4 +1,15 @@
 package za.ac.cput.repository;
 
-public class PatientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.user.Patient;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PatientRepository extends JpaRepository <Patient, Integer> {
+
+    List<Patient> findByDateRegistered(LocalDate dateRegistered);
+
+
+    Patient findByEmail(String email);
 }
