f29c2da3be952249cd22b711e06802de1b13793d
aidan-g-barends <aidanbarends95@gmail.com>
2026-07-31 09:50:23 +0200

    updated the sidebar panel

--- frontend/src/main/java/za/ac/cput/gui/SidebarPanel.java
+++ frontend/src/main/java/za/ac/cput/gui/SidebarPanel.java
@@ -1,6 +1,7 @@
 package za.ac.cput.gui;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class SidebarPanel extends JPanel {
@@ -9,17 +10,21 @@ public class SidebarPanel extends JPanel {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBackground(UITheme.SIDEBAR_WHITE);
         setPreferredSize(new Dimension(220, 0));
+        setBorder(new EmptyBorder(20, 12, 20, 12));
 
-        add(Box.createVerticalStrut(16));
         add(new NavItem("🏠", "Dashboard", true));
+        add(Box.createVerticalStrut(4));
         add(new NavItem("📅", "Appointments", false));
+        add(Box.createVerticalStrut(4));
         add(new NavItem("🎫", "Tickets", false));
+        add(Box.createVerticalStrut(4));
         add(new NavItem("💳", "Payments", false));
+        add(Box.createVerticalStrut(4));
         add(new NavItem("🔔", "Notifications", false));
+        add(Box.createVerticalStrut(4));
         add(new NavItem("👤", "Profile", false));
 
         add(Box.createVerticalGlue());
         add(new NavItem("🚪", "Logout", false));
-        add(Box.createVerticalStrut(16));
     }
 }
